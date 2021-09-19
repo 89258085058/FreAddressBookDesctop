fd17a445a13dae8f3154dc1bc6283ed090495444
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-09-19 11:41:08 +0300

    Задание №26 test

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -5,6 +5,7 @@ import string
 username = "administrator"
 password = "root"
 
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters+string.digits + string.punctuation + " "*10
     return prefix + "".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
--- tests/test_delete_project.py
+++ tests/test_delete_project.py
@@ -5,9 +5,14 @@ import string
 username = "administrator"
 password = "root"
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters+string.digits + string.punctuation + " "*10
+    return prefix + "".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
+
+
 def test_delete_project(app):
     if len(app.project.get_project_list()) == 0:
-        app.project.create(Project(name="NewName"))
+        app.project.create(Project(name=random_string("NewName: ", 10)))
     old_projects = app.soap.get_project_list(username, password)
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.id)
